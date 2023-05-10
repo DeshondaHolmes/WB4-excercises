0bdbbc62ac3b65ddf68394f252b2854c67b19f3f
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-05-10 19:55:16 -0400

    Add lecture demos

---
+++ lecturedemos/2-37.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+
+let names = ["zeph", "anderson", "Zachary", "Adam"];
+
+
+let products = [
+    {prodId: 2, item: "Notepads (12 pk)", price: 12.29},
+    {prodId: 12, item: "Black Pens (12 pk)", price: 5.70},
+    {prodId: 22, item: "Stapler", price: 12.79}
+   ];
+
+
+   [ 80, 83, 86, 92, 100] 
